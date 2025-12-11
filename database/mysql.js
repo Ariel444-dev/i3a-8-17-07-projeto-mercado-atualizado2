8286afbc160a3d8ea452129a5eb5f94e79a77896
Ariel444-dev <rodriguesamelliaii@gmail.com>
2025-12-11 17:08:09 -0300

    Tentando1vincularRender

--- database/mysql.js
+++ database/mysql.js
@@ -1,14 +1,29 @@
 import { Sequelize } from "sequelize";
-import mysql2 from 'mysql2';
+
+import pg from 'pg';
 
 const mysql = new Sequelize({
-    dialect: 'mysql',
-    dialectModule: mysql2,
-    host: 'localhost',
-    port: '3306',
-    database: 'mercadoa',
-    username: 'root',
-    password: 'root'
+dialect: 'postgres',
+dialectModule: pg,
+host: 'dpg-d4omnlidbo4c73f7e7ug-a',
+port: '5432',
+database: 'database_projeto_i3a_wed7',
+username: 'user_database_projeto_i3a',
+password: 'MMbBkpGiqMoGCscwoOoCJJJOAYB8MlSa'
 });
+
+/*
+import mysql2 from "mysql2";
+
+const mysql = new Sequelize({
+dialect: 'mysql',
+dialectModule: mysql2,
+host: 'localhost',
+port: '3306',
+database: 'mercadoa',
+username: 'root',
+password: 'root'
+});
+*/
 
 export default mysql;
