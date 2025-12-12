ecb9dc9ad6dc66e8e598836db236edeb9983e3a3
Ariel444-dev <rodriguesamelliaii@gmail.com>
2025-12-12 00:03:41 -0300

    1.1

--- app/layout.js
+++ app/layout.js
@@ -28,3 +28,5 @@ export default function RootLayout({ children }) {
 
   )
 }
+
+export const dynamic = "force-dynamic";
--- database/mysql.js
+++ database/mysql.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
-
 import pg from 'pg';
+
 
 const mysql = new Sequelize({
 dialect: 'postgres',
